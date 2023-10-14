77ecac78eb162377bcef6e6d77b2a1bbdfb97913
ayeshasikander <sikanderayesha46@gmail.com>
2023-10-14 16:03:29 +0500

    animated page using gsap

---
+++ script.js
@@ -0,0 +1,20 @@
+var tl = gsap.timeline({ repeat: -1 });
+
+tl
+    .to(".img-section", {
+        width: "100%",
+        ease: Expo.easeInOut,
+        stagger: 2
+
+    }, 'a')
+    .to(".text h1", {
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: 0
+    }, 'a')
+    .to(".text h1", {
+        delay: 2,
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: "-100%"
+    }, 'a')
